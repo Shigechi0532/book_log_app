44f0b2ada96735b3bd54f7481893cf5cb123287c
成朗 栁川 <twilight.ff000@gmail.com>
2026-03-27 21:59:00 +0900

    メイン画面土台作成

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,41 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const MaterialApp(home: MainPage());
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('読書記録')),
+      body: Row(
+        children: [
+          // サイドメニュー（左側）
+          NavigationRail(
+            selectedIndex: 0,
+            onDestinationSelected: (index) {},
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.person),
+                label: Text('プロフィール'),
+              ),
+            ],
+          ),
+
+          // メインコンテンツ
+          Expanded(
+            child: ListView(
+              children: List.generate(
+                30,
+                (index) => ListTile(title: Text('アイテム $index')),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
